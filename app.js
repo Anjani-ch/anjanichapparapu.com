1dd3c240062106cb7ef29968b92a76143115c581
Anjani <pranav.ch.12@outlook.com>
2022-01-30 18:35:58 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -12,5 +12,6 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => res.render('index', { title: 'Home', year: new Date().getFullYear() }));
 app.get('/job-experience', (req, res) => res.render('job-experience', { title: 'Job Experience', year: new Date().getFullYear() }));
 app.get('/education', (req, res) => res.render('education', { title: 'Education', year: new Date().getFullYear() }));
+app.get('/projects', (req, res) => res.render('projects', { title: 'Projects', year: new Date().getFullYear() }));
 
 app.listen(PORT, _ => console.log(`Server running on port ${PORT}`));
