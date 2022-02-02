e5e76eb93535e7d2fe4ac3b56391a9ea504c8c20
Anjani <pranav.ch.12@outlook.com>
2022-02-02 12:57:13 +0100

    Finished Project

--- app.js
+++ app.js
@@ -13,5 +13,6 @@ app.get('/', (req, res) => res.render('index', { title: 'Home', year: new Date()
 app.get('/job-experience', (req, res) => res.render('job-experience', { title: 'Job Experience', year: new Date().getFullYear() }));
 app.get('/education', (req, res) => res.render('education', { title: 'Education', year: new Date().getFullYear() }));
 app.get('/projects', (req, res) => res.render('projects', { title: 'Projects', year: new Date().getFullYear() }));
+app.get('/download/resume', (req, res) => res.download('src/docs/cv.pdf'));
 
 app.listen(PORT, _ => console.log(`Server running on port ${PORT}`));
