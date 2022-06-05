a82dedcd58b96eb15fe072f114d5df6b1ba8a79e
Anjani <pranav.ch.12@outlook.com>
2022-06-05 10:14:37 +0200

    Updated tailwind workflow

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer class="p-4 mt-20 font-medium md:p-6">
-        <p class="text-center text-gray-700">
+    <footer class="p-4 font-medium md:p-6">
+        <p class="text-center">
             &copy; 2022 Anjani Chapparapu. All Rights Reserved.
         </p>
     </footer>
