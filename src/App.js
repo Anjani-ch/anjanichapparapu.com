5c590f4f9d6f534f1a238c2da96f515d0d4bd1e3
Anjani <pranav.ch.12@outlook.com>
2022-06-05 11:09:43 +0200

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage.js';
+import AboutPage from './pages/AboutPage.js';
+import ProjectsPage from './pages/ProjectsPage.js';
+import ContactPage from './pages/ContactPage.js';
 
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
@@ -7,8 +13,13 @@ function App() {
   return (
     <Fragment>
       <Header />
-        <main>
-          <h1>WEB</h1>
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
         </main>
       <Footer />
     </Fragment>
