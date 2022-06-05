a6fade54a2e0f0dd7862f3cc91ca717ff16f6a02
Anjani <pranav.ch.12@outlook.com>
2022-06-05 13:24:50 +0200

    Added dynamic year to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="p-4 font-medium border-t border-slate-400 md:p-6 dark:bg-gray-800">
-        <p className="text-center">
-          &copy; 2022 Anjani Chapparapu. All Rights Reserved.
-        </p>
+    <footer className="p-4 font-medium border-t border-slate-400 text-center md:p-6 dark:bg-gray-800">
+      &copy; {new Date().getFullYear()} Anjani Chapparapu. All Rights Reserved.
     </footer>
   )
 }
