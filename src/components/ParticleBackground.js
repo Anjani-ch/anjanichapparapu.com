05e62a9a6ae590f82421ffdbb8f85da9ff2a9cc0
Anjani <pranav.ch.12@outlook.com>
2022-06-06 18:11:44 +0200

    Adde particle system background to home

--- src/components/ParticleBackground.js
+++ src/components/ParticleBackground.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
+
+import ParticleSystem from '../classes/ParticleSystem';
+import Particle from '../classes/Particle';
 
 function ParticleBackground() {
+  const canvasRef = useRef(null);
+
+  const initParticleBackground = () => {
+    const NUMBER_OF_PARTICLES = 80;
+
+    const canvas = canvasRef.current;
+
+    const canvasWidth = canvas.offsetWidth;
+    const canvasHeight = canvas.offsetHeight;
+
+    const particles = [];
+
+    const particleColor = document.documentElement.classList.contains('dark') ? 'white' : '#2563EB';
+
+    let particleSystem;
+
+    for(let i = 0; i < NUMBER_OF_PARTICLES; i++) {
+        const PARTICLE_RADIUS = 2;
+
+        const x = Math.round(Math.random() * canvasWidth);
+        const y = Math.round(Math.random() * canvasHeight);
+
+        const particle = new Particle(x, y, PARTICLE_RADIUS, particleColor);
+
+        particles.push(particle);
+    }
+
+    particleSystem = new ParticleSystem(canvasWidth, canvasHeight, particles, canvas);
+
+    particleSystem.run();
+  }
+
+  useEffect(initParticleBackground, []);
+
+  useEffect(() => {
+      console.log(123);
+  });
+
   return (
-    <canvas className="absolute top-0 left-0 w-full h-full"></canvas>
+    <canvas ref={canvasRef} className="absolute top-0 left-0 w-full h-full hidden md:block"></canvas>
   )
 }
 
 export default ParticleBackground;
 
-// https://stackoverflow.com/questions/14684393/is-there-a-way-to-get-the-hexadecimal-value-of-any-css-color
+// https://stackoverflow.com/questions/14684393/is-there-a-way-to-get-the-hexadecimal-value-of-any-css-coloryoutube.com
