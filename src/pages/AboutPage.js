45ef9ad9b5abbba806bc7b0ebb4ffb3fbaf83e30
Anjani <pranav.ch.12@outlook.com>
2022-06-08 19:07:52 +0200

    Added dark mode border color to about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -9,11 +9,11 @@ function AboutPage() {
       <h1 className="text-4xl text-center mb-10">About <span className="text-blue-600 dark:text-blue-500">Me</span></h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-7">
-        <ul>
-          <li className="mb-6 pb-2 border-b-2 border-slate-100"><FontAwesomeIcon className="text-xl mr-3.5" icon={faUser} /> Anjani Pranav Reddy Chapparapu</li>
-          <li className="mb-6 pb-2 border-b-2 border-slate-100"><FontAwesomeIcon className="text-xl mr-3" icon={faEnvelope} /> pranav.ch.12@outlook.com</li>
-          <li className="mb-6 pb-2 border-b-2 border-slate-100"><FontAwesomeIcon className="text-2xl mr-3" icon={faMobileScreen} /> +47 418 41 851</li>
-          <li className="mb-6 pb-2 border-b-2 border-slate-100"><FontAwesomeIcon className="text-2xl mr-2" icon={faGithub} /> <a className="underline" rel="noopener noreferrer" href="https://github.com/Anjani-ch" target="_blank">https://github.com/Anjani-ch</a></li>
+        <ul className="mb-7 md:mb-0">
+          <li className="mb-6 pb-2 border-b-2 border-slate-500"><FontAwesomeIcon className="text-xl mr-3.5" icon={faUser} /> Anjani Pranav Reddy Chapparapu</li>
+          <li className="mb-6 pb-2 border-b-2 border-slate-500"><FontAwesomeIcon className="text-xl mr-3" icon={faEnvelope} /> pranav.ch.12@outlook.com</li>
+          <li className="mb-6 pb-2 border-b-2 border-slate-500"><FontAwesomeIcon className="text-2xl mr-3" icon={faMobileScreen} /> +47 418 41 851</li>
+          <li className="mb-6 pb-2 border-b-2 border-slate-500"><FontAwesomeIcon className="text-2xl mr-2" icon={faGithub} /> <a className="underline" rel="noopener noreferrer" href="https://github.com/Anjani-ch" target="_blank">https://github.com/Anjani-ch</a></li>
         </ul>
 
         <p>I am a person who recently gained an interest in coding, and I was sure that I could turn this into a career. I have learned coding in school and through self-learning. My skills are shown in my work and I am willing to continue to learn and do more work.</p>
