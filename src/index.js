5d414b2997bc448abdcd83f9d4c7e4d3ab04a987
Anjani <pranav.ch.12@outlook.com>
2022-06-09 15:55:25 +0200

    Updated stylesheet import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-import './assets/styles.css';
+import './assets/css/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
