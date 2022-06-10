5ad1b2cf048a567186de048dc1abaf5ca29cd577
Anjani <pranav.ch.12@outlook.com>
2022-06-10 19:29:23 +0200

    Added production build

---
+++ build/precache-manifest.c90addfbab3e804fa8586c7d89d8bc89.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "d59ffbc2285854ccb892",
+    "url": "/static/css/main.62397a2c.chunk.css"
+  },
+  {
+    "revision": "d59ffbc2285854ccb892",
+    "url": "/static/js/main.d59ffbc2.chunk.js"
+  },
+  {
+    "revision": "14cc936dac4440a1a207",
+    "url": "/static/js/1.14cc936d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e0b944f5f2602104db558ebdc8c0596",
+    "url": "/static/media/anjani.7e0b944f.jpg"
+  },
+  {
+    "revision": "3db31354b42e05f52a09785930349076",
+    "url": "/static/media/html.3db31354.svg"
+  },
+  {
+    "revision": "a70a7e3e545f3ddc5707d4c4da8cfa17",
+    "url": "/static/media/css3.a70a7e3e.svg"
+  },
+  {
+    "revision": "ec058d270f65251e04f75c3a71500ea6",
+    "url": "/static/media/javascript.ec058d27.svg"
+  },
+  {
+    "revision": "8e9501eef45841d40c2e037fc1f62442",
+    "url": "/static/media/react.8e9501ee.svg"
+  },
+  {
+    "revision": "f8f879312203cbbbc1e3c5da93c6e60a",
+    "url": "/static/media/vue-dot-js.f8f87931.svg"
+  },
+  {
+    "revision": "bf23308e2cb0f2e1f3e0066cc887edd5",
+    "url": "/static/media/node-js.bf23308e.svg"
+  },
+  {
+    "revision": "e955639abd29a9348e5a56a7ac29011a",
+    "url": "/static/media/mongodb.e955639a.svg"
+  },
+  {
+    "revision": "de626ff8b2eced16013001e7447ccb4f",
+    "url": "/static/media/firebase.de626ff8.svg"
+  },
+  {
+    "revision": "6f4b432cc4943ebab8ad595915222759",
+    "url": "/static/media/git.6f4b432c.svg"
+  },
+  {
+    "revision": "eda9a51da86f9234aa6b02a5010c1438",
+    "url": "/static/media/github.eda9a51d.svg"
+  },
+  {
+    "revision": "fbbc2c683a7ef38542e6ce5a3ddeaa17",
+    "url": "/static/media/linux.fbbc2c68.svg"
+  },
+  {
+    "revision": "c7c10a6927f814b163e6d829b8cab899",
+    "url": "/static/media/vue_movie_app.c7c10a69.png"
+  },
+  {
+    "revision": "7146486d0dc380eafcb1ab98104bcee4",
+    "url": "/static/media/number_guessing_game.7146486d.png"
+  },
+  {
+    "revision": "29663cf72a0473fbff9b72ef87fc50ef",
+    "url": "/static/media/countries_fact_site.29663cf7.png"
+  },
+  {
+    "revision": "c88e8f20b8d4ab90832bd21f4a870123",
+    "url": "/static/media/snake.c88e8f20.png"
+  },
+  {
+    "revision": "7d2433852d709aef0aebac1a642cbf9a",
+    "url": "/static/media/typing_game_words.7d243385.png"
+  },
+  {
+    "revision": "ffeec3dd65958ba964615d2fd150bff6",
+    "url": "/static/media/rock_paper_scissors.ffeec3dd.png"
+  },
+  {
+    "revision": "ddf9817369e0bcbb688232d505ae6f52",
+    "url": "/static/media/simon_game.ddf98173.png"
+  },
+  {
+    "revision": "49bf3c4293b2c704feacf95818180cf9",
+    "url": "/index.html"
+  }
+];
